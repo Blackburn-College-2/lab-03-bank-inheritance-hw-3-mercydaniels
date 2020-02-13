abc63be014331bf15f664fd24410e177a73ecc5a
mercydaniels <mercy.daniels@blackburn.edu>
2020-02-12 20:42:22 -0600

    printing arraylist

--- src/lab03/Account.java
+++ src/lab03/Account.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab03;
+
+import java.util.ArrayList;
 
 /**
  *
@@ -11,23 +9,51 @@ package lab03;
  */
 public class Account {
     private Money balance;
+    private ArrayList<Money> history = new ArrayList<Money>();
     
     public Account(Money balance){
         this.balance = balance;
+        
     }
-    
+    /**
+     * subtracts money from the account balance 
+     * and adds that amount to the history ArrayList
+     * @param m the money to be withdrawn
+     */
     public void withdraw(Money m){
         balance.subtract(m);
+        
+        history.add(balance);
     }
-    
+    /**
+     * adds money to the account balance 
+     * and adds that amount as a negative Money to the history ArrayList
+     * @param m the money to be Deposited
+     */
     public void deposit (Money m){
         balance.add(m);
+        balance.setAmountNeg();
+        history.add(balance);
     }
-    
+    /**
+     * gets the balance value
+     * @return balance as Money
+     */
     public Money getBalance(){
         return balance;
+        
     }
+   
+    /**
+     * 
+     */
     public String toString(){
         return balance.getAmountString();
+    }
+    
+    public void printHistory(){
+        for (int i = 0; i < history.size(); i++) {
+            System.out.println(history.get(i).getAmountString());
+        }
     }
 }
--- src/lab03/Money.java
+++ src/lab03/Money.java
@@ -1,12 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab03;
 
 /**
- *
+ * Money class that takes type currency and an amount
  * @author mercy.daniels
  */
 public class Money {
@@ -18,11 +14,21 @@ public class Money {
         this.currency = currency;
         this.amount = (long) amount;
     }
-
+/**
+ * gets the Amount value for money
+ * @returns amount as long
+ */
     public long getAmount() {
         return amount;
     }
-
+    /**
+     * Sets the amount to a negative value to 
+     * add a withdrawal to the history ArrayList
+     * @return amount as long
+     */
+    public long setAmountNeg() {
+        return -amount;
+    }
     public String getAmountString() {
         return amount + "";
     }
